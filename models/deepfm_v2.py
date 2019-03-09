67ff0d2710733fce2a35706bcbadaf67ef88347b
Yao Zhang <zhangyao998@hotmail.com>
2019-03-09 14:28:06 +0800

    refactor deepfmv2 and add xdeepfm

--- models/deepfm_v2.py
+++ models/deepfm_v2.py
@@ -4,13 +4,81 @@ from torch import nn
 from .field_encoder import FieldEncoder
 
 
+class FM(nn.Module):
+
+    def __init__(self, bias=False):
+        super().__init__()
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor([]))
+            nn.init.zeros_(self.bias.data)
+        else:
+            self.bias = None
+
+    def forward(self, linear_part, embs):
+        # linear_part : [bs, 1, n_fields]
+        # embs : [bs, emb_size, n_fields]
+        order_1_score = linear_part.sum([1, 2])
+        order_2_score = (embs.sum(-1) ** 2).sum(-1) - (embs ** 2).sum(dim=[1, 2])
+        fm_score = order_1_score + 0.5 * order_2_score
+        if self.bias is not None:
+            fm_score += self.bias
+        return fm_score
+
+
+class LinearBlock(nn.Module):
+
+    def __init__(self, in_size, out_size, residual=False,
+                 use_bn=True, dropout=0.5, bias=True, act=None):
+        super().__init__()
+        self.residual = residual if in_size == out_size else False
+        self.bn = nn.BatchNorm1d(in_size) if use_bn else None
+        layers = []
+        if dropout > 0:
+            layers.append(nn.Dropout())
+        layers.append(nn.Linear(in_size, out_size, bias=bias))
+        if act is not None:
+            layers.append(act)
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.bn is not None:
+            if x.dim() == 2:
+                x = self.bn(x)
+            elif x.dim() == 3:
+                x = self.bn(x.transpose(1, 2)).transpose(1, 2)
+            else:
+                raise NotImplementedError
+        z = self.layers(x)
+        if self.residual:
+            z += x
+        return z
+
+
+class PlainDNN(nn.Module):
+
+    def __init__(self, input_size, net_dims, residual=False, use_bn=True, dropout=0.5, slope=0.01):
+        super().__init__()
+        net_layers = []
+        prev_dim = input_size
+        for i, d in enumerate(net_dims):
+            dropout_p = 0 if i == 0 else dropout
+            act = nn.LeakyReLU(slope) if i < len(net_dims) - 1 else None
+            net_layers.append(LinearBlock(prev_dim, d, residual=residual, use_bn=use_bn,
+                                          dropout=dropout_p, act=act))
+            prev_dim = d
+        self.net_layers = nn.Sequential(*net_layers)
+
+    def forward(self, x):
+        return self.net_layers(x)
+
+
 class DeepFMv2(nn.Module):
 
     def __init__(self, emb_size, net_dims,
                  onehot_vocab_sizes=None, sparse_input_sizes=None, dense_input_sizes=None,
                  pretrained_onehot=None, pretrained_sparse=None,
                  pretrained_bias=False, pretrained_finetune=False, padding_idx=None,
-                 use_bn=True, slope=0.2, emb_dropout=0.5, dropout=0.5):
+                 residual=True, use_bn=True, slope=0.2, emb_dropout=0.5, dropout=0.5):
         super().__init__()
         self.encoder = FieldEncoder(emb_size, onehot_vocab_sizes, sparse_input_sizes, dense_input_sizes,
                                     pretrained_onehot, pretrained_sparse,
@@ -20,41 +88,19 @@ class DeepFMv2(nn.Module):
                                            padding_idx=padding_idx, emb_dropout=0.)
 
         self.n_fields = self.encoder.n_fields
-        # Deep
-        net_layers = []
-        prev_dim = emb_size * self.n_fields
-        for i, d in enumerate(net_dims):
-            if use_bn:
-                net_layers.append(nn.BatchNorm1d(prev_dim))
-            if i != 0:
-                net_layers.append(nn.Dropout(dropout))
-            net_layers.append(nn.Linear(prev_dim, d))
-            prev_dim = d
-            if i < len(net_dims) - 1:
-                net_layers.append(nn.LeakyReLU(slope))
-        self.net_layers = nn.Sequential(*net_layers)
+        self.fm_layer = FM(False)
+        self.deep_layer = PlainDNN(emb_size * self.n_fields, net_dims=net_dims,
+                                   residual=residual, use_bn=use_bn,
+                                   dropout=dropout, slope=slope)
 
-        # Global Bias
-        self.global_bias = nn.Parameter(torch.FloatTensor(net_dims[-1]))
-        nn.init.zeros_(self.global_bias.data)
-
-    def forward(self, ids, sparse_xs, dense_xs):
+    def forward(self, ids=None, sparse_xs=None, dense_xs=None):
         embs = self.encoder(ids, sparse_xs, dense_xs)
         # FM Part
         linear_part = self.linear_encoder(ids, sparse_xs, dense_xs)
-        fm_score = calc_fm_score(linear_part, embs)
+        fm_score = self.fm_layer(linear_part, embs)
         # Deep Part
         bs = embs.size(0)
-        z = self.net_layers(embs.view(bs, -1))  # [bs, output_size]
+        z = self.deep_layer(embs.view(bs, -1))  # [bs, output_size]
 
-        z = z + fm_score.unsqueeze(1) + self.global_bias
+        z = z + fm_score.unsqueeze(1)
         return z
-
-
-def calc_fm_score(linear_part, embs):
-    # linear_part : [bs, 1, n_fields]
-    # embs : [bs, emb_size, n_fields]
-    order_1_score = linear_part.sum([1, 2])
-    order_2_score = (embs.sum(-1) ** 2).sum(-1) - (embs ** 2).sum(dim=[1, 2])
-    fm_score = order_1_score + 0.5 * order_2_score
-    return fm_score
---
+++ models/xdeepfm.py
@@ -0,0 +1,103 @@
+import torch
+from torch import nn
+
+from models.field_encoder import FieldEncoder
+from models.deepfm_v2 import LinearBlock, FM, PlainDNN
+
+
+class CINLayer(nn.Module):
+
+    def __init__(self, n_fields, m_fields, o_fields, dropout=0., use_bn=True, bias=False):
+        super().__init__()
+        self.layer = LinearBlock(n_fields * m_fields, o_fields, residual=False,
+                                 use_bn=use_bn, dropout=dropout, act=None, bias=bias)
+
+    def forward(self, x, y):
+        # x : [bs, dim, n]
+        # y : [bs, dim, m]
+        bs, dim, _ = x.size()
+        z = x.unsqueeze(-1) @ y.unsqueeze(-2)  # [bs, dim, n, m]
+        z = z.view([bs, dim, -1])
+        z = self.layer(z)  # [bs, dim, o]
+        return z
+
+
+class CIN(nn.Module):
+
+    def __init__(self, n_fields, net_fields, in_size, out_size,
+                 bias=False, use_act=False, slope=0.01, dropout=0.,
+                 use_bn=True):
+        super().__init__()
+        net_fields = [n_fields] + net_fields
+        layers = []
+        for i in range(len(net_fields) - 1):
+            m, o = net_fields[i], net_fields[i+1]
+            layers.append(CINLayer(n_fields, m, o,
+                                   dropout=dropout,
+                                   use_bn=use_bn,
+                                   bias=bias))
+        self.layers = nn.ModuleList(layers)
+        if use_act:
+            self.act = nn.LeakyReLU(slope)
+        else:
+            self.act = None
+        self.linear = LinearBlock(in_size * (len(net_fields)-1), out_size,
+                                  residual=False, use_bn=use_bn, dropout=dropout)
+
+    def forward(self, x):
+        # x : [bs, dim, n]
+        prev_x = x
+        ps = []
+        for l in self.layers:
+            prev_x = l(x, prev_x)  # [bs, dim, ?]
+            if self.act is not None:
+                prev_x = self.act(prev_x)
+            ps.append(prev_x.sum(dim=-1))
+        ps = torch.cat(ps, dim=-1)  # [bs, dim * n_layers]
+        z = self.linear(ps)
+        return z
+
+
+class xDeepFM(nn.Module):
+
+    def __init__(self, emb_size, net_dims, cin_fields,
+                 onehot_vocab_sizes=None, sparse_input_sizes=None, dense_input_sizes=None,
+                 pretrained_onehot=None, pretrained_sparse=None,
+                 pretrained_bias=False, pretrained_finetune=False, padding_idx=None,
+                 cin_bias=False, cin_use_act=False, use_fm=False,
+                 residual=True, use_bn=True, slope=0.2, emb_dropout=0.5, dropout=0.5):
+        super().__init__()
+        self.encoder = FieldEncoder(emb_size, onehot_vocab_sizes, sparse_input_sizes, dense_input_sizes,
+                                    pretrained_onehot, pretrained_sparse,
+                                    pretrained_bias, pretrained_finetune,
+                                    padding_idx, emb_dropout)
+        self.n_fields = self.encoder.n_fields
+
+        self.use_fm = use_fm
+        if use_fm:
+            self.linear_encoder = FieldEncoder(1, onehot_vocab_sizes, sparse_input_sizes, dense_input_sizes,
+                                               padding_idx=padding_idx, emb_dropout=0.)
+            self.fm_layer = FM(False)
+        self.deep_layer = PlainDNN(emb_size * self.n_fields, net_dims=net_dims,
+                                   residual=residual, use_bn=use_bn,
+                                   dropout=dropout, slope=slope)
+        self.cin_layer = CIN(self.n_fields, cin_fields, in_size=emb_size, out_size=net_dims[-1],
+                             bias=cin_bias, use_act=cin_use_act, slope=slope, dropout=dropout, use_bn=use_bn)
+
+    def forward(self, ids=None, sparse_xs=None, dense_xs=None):
+        embs = self.encoder(ids, sparse_xs, dense_xs)
+        # Deep Part
+        bs = embs.size(0)
+        z_deep = self.deep_layer(embs.view(bs, -1))  # [bs, output_size]
+
+        # CIN
+        z_cin = self.cin_layer(embs)
+
+        if self.use_fm:
+            # FM Part
+            linear_part = self.linear_encoder(ids, sparse_xs, dense_xs)
+            fm_score = self.fm_layer(linear_part, embs)
+            z = z_deep + z_cin + fm_score
+        else:
+            z = z_deep + z_cin
+        return z
