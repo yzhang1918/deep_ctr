c389508b3a4351b91565d473c56fe94edcbbaa21
Yao Zhang <zhangyao998@hotmail.com>
2019-03-14 17:35:14 +0800

    add tiktok item loader and update splinear layer

--- models/layers.py
+++ models/layers.py
@@ -6,7 +6,11 @@ from torch_sparse import spmm
 class SparseLinear(nn.Linear):
 
     def forward(self, x):
-        y = spmm(x._indices(), x._values(), x.size(0), self.weight.t())
+        if isinstance(x, list) or isinstance(x, tuple):
+            ind, val = x
+        else:
+            ind, val = x._indices(), x._values()
+        y = spmm(ind, val, x.size(0), self.weight.t())
         if self.bias is not None:
             y += self.bias
         return y
