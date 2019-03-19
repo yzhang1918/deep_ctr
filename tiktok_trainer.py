443338add05e106a113336fa72cff41158f78a69
Yao Zhang <zhangyao998@hotmail.com>
2019-03-19 23:10:00 +0800

    beautify tqdm

--- tiktok_trainer.py
+++ tiktok_trainer.py
@@ -36,9 +36,10 @@ class TikTokTrainer(BaseTrainer):
     def train(self, i_epoch=None, scheduler=None):
         self.model.train()
         cum_loss = None
-        with tqdm.tqdm(total=len(self.train_dl)) as t:
+        total = self.break_at if self.break_at > 0 else len(self.train_dl)
+        with tqdm.tqdm(total=total) as t:
             if i_epoch is not None:
-                t.set_description(f'[Epoch {i_epoch + 1:3d}]')
+                t.set_description(f'[Epoch {i_epoch + 1:4d}]')
             for i, batch in enumerate(self.train_dl):
                 if scheduler is not None:
                     scheduler.step()
@@ -54,28 +55,32 @@ class TikTokTrainer(BaseTrainer):
                 else:
                     cum_loss = self.smooth_factor * cum_loss + (1 - self.smooth_factor) * loss.item()
 
-                t.set_postfix(loss=f'{cum_loss:.3f}')
+                t.set_postfix(loss=f'{loss:.3f}')
                 t.update()
-                if 0 < self.break_at < i:
+                if 0 < self.break_at <= i+1:
                     return
 
     def valid(self):
         self.model.eval()
         all_preds = []
         all_targets = []
+        cum_loss = 0.
+        total = self.break_at if self.break_at > 0 else len(self.valid_dl)
         with torch.no_grad():
-            cum_loss = 0.
-            for i, batch in tqdm.tqdm(enumerate(self.valid_dl), total=len(self.valid_dl)):
-                x, labels = batch.cuda()
-                preds = self.model(*x)
-                loss = self.criterion(preds, labels)
-                cum_loss += loss.item()
-                all_preds.append(preds.cpu().numpy())
-                all_targets.append(labels.cpu().numpy())
-                if 0 < self.break_at < i:
-                    break
-        n = len(self.valid_dl)
-        cum_loss /= n
+            with tqdm.tqdm(total=total) as t:
+                t.set_description('[Validation]')
+                for i, batch in enumerate(self.valid_dl):
+                    x, labels = batch.cuda()
+                    preds = self.model(*x)
+                    loss = self.criterion(preds, labels)
+                    cum_loss += loss.item()
+                    all_preds.append(preds.cpu().numpy())
+                    all_targets.append(labels.cpu().numpy())
+                    t.set_postfix(loss='0.000')
+                    t.update()
+                    if 0 < self.break_at <= i+1:
+                        break
+        cum_loss /= total
         all_preds = np.concatenate(all_preds)
         all_targets = np.concatenate(all_targets)
         finish_auc, like_auc = self.calc_auc(all_preds, all_targets)
@@ -86,12 +91,16 @@ class TikTokTrainer(BaseTrainer):
     def test(self):
         self.model.eval()
         all_preds = []
+        total = self.break_at if self.break_at else len(self.test_dl)
         with torch.no_grad():
-            for i, batch in tqdm.tqdm(enumerate(self.test_dl), total=len(self.test_dl)):
-                x, _ = batch.cuda()
-                preds = self.model(*x)
-                all_preds.append(preds.cpu().numpy())
-                if 0 < self.break_at < i:
-                    break
+            with tqdm.tqdm(total=total) as t:
+                for i, batch in enumerate(self.test_dl):
+                    x, _ = batch.cuda()
+                    preds = self.model(*x)
+                    all_preds.append(preds.cpu().numpy())
+                    t.set_postfix(loss='0.000')
+                    t.update()
+                    if 0 < self.break_at <= i+1:
+                        break
         all_preds = np.concatenate(all_preds)
         return all_preds
